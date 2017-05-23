d5a1460bdaaa12558822264acbdfd65abb0b40dc
Evliya Celebi <motto2cat@gmail.com>
2017-05-24 00:32:24 +0300

    revized using count on product listing

---
+++ spec/features/product_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+RSpec.describe "Product", type: :feature do
+  
+
+  it "creates a new product" do
+    visit new_product_path
+    expect {
+      fill_in "product_name", with: "The Product Name"
+      fill_in "product_brand" , with: "The Brand"
+      click_button "Create Product"
+    }.to change(Product, :count).by(1)
+  end
+
+  it "seems on product index page" do
+    create :product, name: "The Product Name"
+    visit products_path
+    expect(page).to have_content("The Product Name")
+  end
+
+  it "shows product using count on index page" do
+    user = create :user
+    product = create :product
+    approved_image = create :approval_image, product: product, user: user
+    create :usage_manifest, user: user, product: product, status: :using, approved_image: nil 
+    create :usage_manifest, user: user, product: product, status: :using, approved_image: approved_image
+
+    visit products_path
+    expect(page).to have_content("1 using")
+  end
+
+  it "shows product used count on index page" do
+    user = create :user
+    product = create :product
+
+    2.times do
+      create :usage_manifest, user: user, product: product, status: :used
+    end
+
+    visit products_path
+    expect(page).to have_content("2 used")
+  end
+
+
+end
