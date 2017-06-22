2b4cac3c69fc95bc5ceee179402eb2e52dc874b8
Evliya Celebi <motto2cat@gmail.com>
2017-06-22 05:46:32 +0300

    configurations for carriewave production

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,19 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+      host:                  ENV['AWS_S3_ASSET_URL'],
+    }
+    config.fog_directory  = ENV['AWS_S3_BUCKET_NAME']
+    config.fog_public     = false
+    config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
+  else
+    config.storage = :file
+    config.enable_processing = false
+  end
+
+end
