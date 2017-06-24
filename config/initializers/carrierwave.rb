6cd906ff672f343d7d46d2a965a8d9cda632e451
Evliya Celebi <motto2cat@gmail.com>
2017-06-24 17:50:38 +0300

    bug fix: aws host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
       provider:              'AWS',
       aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
       aws_secret_access_key: ENV['AWS_SECRET_KEY'],
-      host:                  ENV['AWS_S3_ASSET_URL'],
+      host:                  "#{ENV['AWS_S3_ASSET_URL']}/#{ENV['AWS_S3_BUCKET_NAME']}",
       region:                ENV['AWS_S3_REGION']
     }
     config.fog_directory  = ENV['AWS_S3_BUCKET_NAME']
