5cfdf052dae4f316fff7e8e57682bb796132d1d1
Evliya Celebi <motto2cat@gmail.com>
2017-06-24 06:19:07 +0300

    add region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
       aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
       aws_secret_access_key: ENV['AWS_SECRET_KEY'],
       host:                  ENV['AWS_S3_ASSET_URL'],
+      region:                ENV['AWS_S3_REGION']
     }
     config.fog_directory  = ENV['AWS_S3_BUCKET_NAME']
     config.fog_public     = false
