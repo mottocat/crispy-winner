60b47bc77ace23dc48a39f346f03e9f49233f6f5
Evliya Celebi <motto2cat@gmail.com>
2017-07-04 18:24:50 +0300

    google analytics sends pageview after turbolinks loaded

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,10 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+document.addEventListener('turbolinks:load', function(event) {
+  if (typeof ga === 'function') {
+    ga('set', 'location', event.data.url);
+    ga('send', 'pageview');
+  }
+});
